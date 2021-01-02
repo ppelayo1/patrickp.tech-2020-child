de9cd5363fdd575d6203fb8de9627bcb47a3fcbe
Patrick Pelayo <PatrickP@PatrickP.Tech>
2021-01-02 12:54:38 -0800

    made header smaller so as to keep focal point on portfolio

---
+++ header.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PatrickP.Tech</title>
+
+                
+    <?php do_action('wp_head'); ?>
+</head>
+
+<body>
+    <header id='header' class='container-fluid'>
+        <div id='innerHeader' class='container'>
+            <h1><a href="<?php home_url(); ?>"><?php bloginfo('name'); ?> </a></h1>
+        </div>
+    </header>
+
+    
