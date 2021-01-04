e05fd59924f4a95a8b653c59a1924cc3c1bb5b03
Patrick Pelayo <PatrickP@PatrickP.Tech>
2021-01-03 16:01:32 -0800

    updating the footer to match my 3rd design of footer in photo

--- footer.php
+++ footer.php
@@ -5,23 +5,21 @@
     </div>
 
     <footer class='container-fluid'>
-        <div class='container'>
-            <div class='text-center'>
-                <h1 class='playFairFont'>Contact Me</h1>
-                <p><a href="mailto:PatrickP@PatrickP.Tech?Subject=Enter%20Subject" target="_top">PatrickP@PatrickP.Tech</a></p>
-            </div>
-            <div class='row'>
-                <div class='col-sm-6 text-center'>
-                    <h2 class='playFairFont'>Location</h2>
-                    <img class='mt-3 mb-5 img-fluid ' src="<?php echo get_template_directory_uri() . '/img/usflag.jpg'?>" alt='usFlag'/>
-                    
-                </div>
-                <div class='col-sm-6 text-center'>
+        <div id='innerFooter' class='container'>
+            <div class='row text-left'>
+                <div id='contactMeFollowMeWrapper' class='col-sm-5'>
+                    <div>
+                        <h2 class='playFairFont'>Contact Me</h2>
+                        <p><a href="mailto:PatrickP@PatrickP.Tech?Subject=Enter%20Subject" target="_top">PatrickP@PatrickP.Tech</a></p>
+                    </div>
                     <h2 class='playFairFont'>Follow Me</h2>
-                    <ul class='list-unstyled'>
+                    <ul id='socialMediaLinks' class='list-unstyled'>
                         <li><a href="https://www.facebook.com/patrick.pelayo.733450" target="_blank"><i class="socialIcon fab fa-facebook-square"></i></a></li>
                         <li><a href="https://www.linkedin.com/in/patrick-pelayo-91b778161/" target="_blank"><i class= "socialIcon fab fa-linkedin"></i></a></li>                
                     </ul>
+                </div>
+                <div class='col-sm-7'>
+                    <img id='flagImg' class='img-fluid ' src="<?php echo get_template_directory_uri() . '/img/usflag.jpg'?>" alt='usFlag'/>
                 </div>
             </div>
             <p class='playFairFont'>&copy;<?php echo date("Y"); ?> PatrickP.Tech  </p>
